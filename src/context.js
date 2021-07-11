45a56c26eb49bde7bc0849db1a55d4e2a340dce8
Glen Maytum <Glen@Glens-Mac-mini.local>
2021-07-11 14:39:10 +0100

    finish project

--- src/context.js
+++ src/context.js
@@ -22,7 +22,11 @@ const AppProvider = ({ children }) => {
 	const [index, setIndex] = useState(0);
 	const [correct, setCorrect] = useState(0);
 	const [error, setError] = useState(false);
-
+	const [quiz, setQuiz] = useState({
+		amount: 10,
+		category: "sports",
+		difficulty: "easy",
+	});
 	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	const fetchQuestions = async (url) => {
@@ -46,9 +50,47 @@ const AppProvider = ({ children }) => {
 		}
 	};
 
-	useEffect(() => {
-		fetchQuestions(tempUrl);
-	}, []);
+	const nextQuestion = () => {
+		setIndex((oldIndex) => {
+			const index = oldIndex + 1;
+			if (index > questions.length - 1) {
+				openModal();
+				return 0;
+			} else {
+				return index;
+			}
+		});
+	};
+	const checkAnswer = (value) => {
+		if (value) {
+			setCorrect((oldState) => oldState + 1);
+		}
+		nextQuestion();
+	};
+
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
+	const closeModal = () => {
+		setWaiting(true);
+		setCorrect(0);
+		setIsModalOpen(false);
+	};
+
+	const handleChange = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		setQuiz({ ...quiz, [name]: value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const { amount, category, difficulty } = quiz;
+
+		const url = `${API_ENDPOINT}amount=${amount}&difficulty=${difficulty}&category=${table[category]}&type=multiple`;
+		fetchQuestions(url);
+	};
+
 	return (
 		<AppContext.Provider
 			value={{
@@ -59,6 +101,12 @@ const AppProvider = ({ children }) => {
 				correct,
 				error,
 				isModalOpen,
+				nextQuestion,
+				checkAnswer,
+				closeModal,
+				quiz,
+				handleChange,
+				handleSubmit,
 			}}
 		>
 			{children}
